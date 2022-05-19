aa74dbe6da7956737ae5baf19923578379b87664
Adalberto Teshima Júnior <teshima.jr@hotmail.com>
2022-05-19 17:38:57 -0300

    Projeto Finalizado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,14 +4,10 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'bytebank';
 
   constructor(private service: TransferenciaService) {}
-
-  transferir($event) {
-    this.service.adicionar($event)
-  }
 }
--- src/app/nova-transferencia/nova-transferencia.component.ts
+++ src/app/nova-transferencia/nova-transferencia.component.ts
@@ -1,22 +1,35 @@
+import { TransferenciaService } from './../services/transferencia.service';
+import { HttpClient } from '@angular/common/http';
 import { Component, EventEmitter, Output } from '@angular/core';
+import { Transferencia } from '../models/transferencia.model';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-nova-transferencia',
   templateUrl: './nova-transferencia.component.html',
   styleUrls: ['./nova-transferencia.component.scss'],
 })
-
 export class NovaTransferenciaComponent {
-
   @Output() aoTransferir = new EventEmitter<any>();
 
   valor: number;
   destino: number;
 
+  constructor(private service: TransferenciaService, private router: Router) {}
+
   transferir() {
-    const valorEmitir = { valor: this.valor, destino: this.destino }
-    this.aoTransferir.emit(valorEmitir);
-    this.limparCampos();
+    const valorEmitir: Transferencia = {
+      valor: this.valor,
+      destino: this.destino,
+    };
+
+    this.service.adicionar(valorEmitir).subscribe(
+      (resultado) => {
+        console.log(resultado);
+        this.router.navigateByUrl('extrato');
+      },
+      (error) => console.error(error)
+    );
   }
 
   limparCampos() {
